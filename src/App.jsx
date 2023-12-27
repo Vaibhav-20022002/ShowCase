95fe99d54308abadf7d87d6839c9f55024d1f291
Vaibhav-20022002 <vibhu20022002@gmail.com>
2023-12-27 19:04:24 +0530

    Added redux with redux toolkit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import { useState, useEffect } from "react";
 import fetchDataFromAPI from "./utils/api";
 
+import { useSelector, useDispatch } from "react-redux";
+import { getApiConfiguration } from "./store/homeSlice";
+
 function App() {
+	const dispatch = useDispatch();
+	const url = useSelector((state) => state.home.url);
+
 	function testing() {
 		fetchDataFromAPI("/movie/popular").then((res) => {
 			console.log(res);
+			dispatch(getApiConfiguration(res));
 		});
 	}
 
@@ -12,7 +19,7 @@ function App() {
 		testing();
 	}, []);
 
-	return <div>App</div>;
+	return <div>App url.total_pages = {url.total_pages}</div>;
 }
 
 export default App;
