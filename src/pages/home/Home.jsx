996d36d3f36fd9a083badc230e0ffd7d7c6b8f0b
Vaibhav-20022002 <vibhu20022002@gmail.com>
2024-01-04 22:16:52 +0530

    Added Switch Tabs successfully

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,13 +1,16 @@
 import React from "react";
+
 import "./home.scss";
 
+import Trending from "./trending/Trending";
 import HeroBanner from "./heroBanner/HeroBanner";
 
 const Home = () => {
 	return (
-		<>
+		<div className="homePage">
 			<HeroBanner />
-		</>
+			<Trending />
+		</div>
 	);
 };
 
---
+++ src/pages/home/trending/Trending.jsx
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+
+import "./trending.scss";
+
+import ContentWrapper from "../../../components/contentWrapper/ContentWrapper";
+import SwitchTabs from "../../../components/switchTabs/SwitchTabs";
+import useFetch from "../../../hooks/useFetch";
+
+const Trending = () => {
+	const [endpoint, setEndpoint] = useState("day");
+
+	const onTabChange = (tab) => {
+		setEndpoint(tab === "Day" ? "day" : "week");
+	};
+
+	const { data, loading } = useFetch(`/trending/all/${endpoint}`);
+
+	return (
+		<div className="carouselSection">
+			<ContentWrapper>
+				<span className="carouselTitle">Trending</span>
+				<SwitchTabs data={["Day", "Week"]} onTabChange={onTabChange} />
+			</ContentWrapper>
+		</div>
+	);
+};
+
+export default Trending;
