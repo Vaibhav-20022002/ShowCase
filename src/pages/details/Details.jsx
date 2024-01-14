397d2c93a92cc2154b0c63684315d9d9db2b370c
Vaibhav-20022002 <vibhu20022002@gmail.com>
2024-01-15 01:02:28 +0530

    Added DetailsBanner successfully(video popup and other details)

--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -2,11 +2,19 @@ import React from "react";
 
 import "./details.scss";
 import DetailsBanner from "./detailBanner/DetailBanner";
+import { useParams } from "react-router-dom";
+import useFetch from "../../hooks/useFetch";
 
 const Details = () => {
+	const { id, mediaType } = useParams();
+	const { data, loading } = useFetch(`/${mediaType}/${id}/videos`);
+	const { data: credits, loading: creditsLoading } = useFetch(
+		`/${mediaType}/${id}/credits`
+	);
+
 	return (
 		<div>
-			<DetailsBanner />
+			<DetailsBanner video={data?.results?.[0]} crew={credits?.crew} />
 		</div>
 	);
 };
