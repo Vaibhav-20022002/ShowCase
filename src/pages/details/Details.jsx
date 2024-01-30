319818719c7725f18f3490271b3559f4f2fcbb73
Vaibhav-20022002 <vibhu20022002@gmail.com>
2024-01-30 15:19:25 +0530

    Added Video Section and Similar Movies to Details page

--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -4,6 +4,10 @@ import "./details.scss";
 import DetailsBanner from "./detailBanner/DetailBanner";
 import { useParams } from "react-router-dom";
 import useFetch from "../../hooks/useFetch";
+import Cast from "./cast/cast";
+import VideosSection from "./videosSection/VideosSection";
+import Similar from "./carousels/Similar";
+import Recommendation from "./carousels/Recommendation";
 
 const Details = () => {
 	const { id, mediaType } = useParams();
@@ -15,6 +19,10 @@ const Details = () => {
 	return (
 		<div>
 			<DetailsBanner video={data?.results?.[0]} crew={credits?.crew} />
+			<Cast data={credits?.cast} loading={loading} />
+			<VideosSection data={data} loading={loading} />
+			<Similar mediaType={mediaType} id={id} />
+			<Recommendation mediaType={mediaType} id={id} />
 		</div>
 	);
 };
